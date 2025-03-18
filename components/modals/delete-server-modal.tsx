56c7413107fd4a126cfcb692493b6b6185206dc1
Omer Faruk Kanlı <kanlomerfaruk@gmail.com>
2025-03-18 16:32:09 +0300

    delete server modal and func added

--- components/modals/delete-server-modal.tsx
+++ components/modals/delete-server-modal.tsx
@@ -13,18 +13,18 @@ import { useState } from 'react';
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
 
-const LeaveServerModal = () => {
+const DeleteServerModal = () => {
   const { isOpen, onClose, type, data } = useModalStore();
   const router = useRouter();
   const { server } = data;
-  const isModalOpen = isOpen && type === 'leaveServer';
+  const isModalOpen = isOpen && type === 'deleteServer';
   const [isLoading, setIsLoading] = useState(false);
 
   const onClick = async () => {
     try {
       setIsLoading(true);
 
-      await axios.patch(`/api/servers/${server?.id}/leave`);
+      await axios.delete(`/api/servers/${server?.id}`);
       onClose();
       router.refresh();
       router.push('/');
@@ -40,14 +40,14 @@ const LeaveServerModal = () => {
       <DialogContent className='bg-white text-black p-0 overflow-hidden'>
         <DialogHeader className='pt-8 px-6'>
           <DialogTitle className='text-2xl text-center font-bold'>
-            Leave Server
+            Delete Server
           </DialogTitle>
           <DialogDescription className='text-center text-zinc-500'>
-            Are you sure you want to leave{' '}
+            Are you sure you want to this <br />
             <span className='font-semibold text-indigo-500'>
               {server?.name}
-            </span>
-            ?
+            </span>{' '}
+            will be deleted permanently.
           </DialogDescription>
         </DialogHeader>
         <DialogFooter className='bg-gray-100 px-6 py-4'>
@@ -65,4 +65,4 @@ const LeaveServerModal = () => {
   );
 };
 
-export default LeaveServerModal;
+export default DeleteServerModal;
--- components/provaiders/modal-provaider.tsx
+++ components/provaiders/modal-provaider.tsx
@@ -5,7 +5,8 @@ import CreateServerModal from '@/components/modals/create-server-modal';
 import EditServerModal from '@/components/modals/edit-server-modal';
 import MemberModal from '@/components/modals/members-modal';
 import CreateChannelModal from '@/components/modals/create-channel-modal';
-import LeaveServerModal from '../modals/leave-server-modal';
+import LeaveServerModal from '@/components/modals/leave-server-modal';
+import DeleteServerModal from '@/components/modals/delete-server-modal';
 
 export const ModalProvider = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -24,6 +25,7 @@ export const ModalProvider = () => {
       <MemberModal />
       <CreateChannelModal />
       <LeaveServerModal />
+      <DeleteServerModal />
     </>
   );
 };
